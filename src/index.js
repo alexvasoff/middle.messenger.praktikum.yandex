b5c1dfae80f1f524788285e1540d5b6138c0b495
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-07-30 18:27:03 +0300

    feat: Add initial indexes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import {sum} from './modules/sum.js';
-const root = document.querySelector('#root');
-root.textContent = sum(6, -1).toString();
+import getPage from "./router";
+
+window.onload = function () {
+    document.getElementById('root').innerHTML = getPage();
+}
