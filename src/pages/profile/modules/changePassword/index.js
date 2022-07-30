46529ac0487d2e62fe2a5e96350339f6851700c3
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-07-30 18:26:09 +0300

    feat: Add profile settings page

---
+++ src/pages/profile/modules/changePassword/index.js
@@ -0,0 +1,19 @@
+import tpl from './tpl.hbs';
+import './style.scss'
+
+import avatar from "../../../../components/avatar";
+import button from "../../../../components/button";
+import input from "../../../../components/input";
+
+export default function changePassword() {
+    const components = {
+        avatar: avatar(),
+        oldPassword: input('oldPassword', 'Старый пароль'),
+        newPassword: input('newPassword', 'Новый пароль'),
+        newPasswordGuard: input('newPasswordGuard', 'Повторите новый пароль'),
+        //
+        savaChanges: button('savaChanges', 'Сохранить изменения'),
+        back: button('back', 'Назад', 'danger-text'),
+    };
+    return tpl(components);
+}
---
+++ src/pages/profile/modules/editData/index.js
@@ -0,0 +1,22 @@
+import tpl from './tpl.hbs';
+import './style.scss'
+
+import avatar from "../../../../components/avatar";
+import button from "../../../../components/button";
+import input from "../../../../components/input";
+
+export default function editData() {
+    const components = {
+        avatar: avatar(),
+        mail: input('email', 'Почта', 'Введите почту'),
+        login: input('login', 'Логин', 'Введите логин'),
+        name: input('first_name', 'Имя', 'Введите имя'),
+        surname: input('second_name', 'Фамилия', 'Введите фамилию'),
+        displayName: input('display_name', 'Имя в чате', 'Введите ник'),
+        phone: input('phone', 'Телефон', 'Введите телефон'),
+        //
+        savaChanges: button('savaChanges', 'Сохранить изменения'),
+        back: button('back', 'Назад', 'danger-text'),
+    };
+    return tpl(components);
+}
