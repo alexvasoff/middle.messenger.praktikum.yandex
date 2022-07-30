ce715316e34c3ef6b4ae57a35d9899e22ff24a67
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-07-30 18:26:44 +0300

    feat: Add router

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import login from "../pages/login";
+import registration from "../pages/registration";
+import chat from "../pages/chat";
+import profile from "../pages/profile";
+import editData from "../pages/profile/modules/editData";
+import changePassword from "../pages/profile/modules/changePassword";
+import notFound from "../pages/notFound";
+import serverError from "../pages/serverError";
+
+const routes = {
+    '/': login,
+    '/register': registration,
+    '/chat': chat,
+    '/settings': profile,
+    '/changeData': editData,
+    '/changePassword': changePassword,
+    '/notFound':notFound,
+    '/serverError': serverError,
+}
+
+function getPage() {
+    const currentPath = window.location.pathname;
+    let page = routes[currentPath];
+    return page() || routes['/notFound']();
+}
+
+export default getPage;
