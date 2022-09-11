a9f773224017c3303f133851dd9e965c0e86d731
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-09-11 22:42:16 +0300

    feat: add ButtonBlock

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,16 @@
 import tpl from './tpl.hbs';
 import './style.scss';
+import { BaseBlock } from '../../utils/baseBlock';
+
+export class Button extends BaseBlock {
+  constructor(props) {
+    super('div', props);
+  }
+
+  render() {
+    return tpl(this.props);
+  }
+}
 
 export function button(name, text, type = 'default') {
   return tpl({ name, text, type });
