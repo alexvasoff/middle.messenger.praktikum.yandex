b1e8d02d6fcd7624ae42e0e0043d0a67bcd259dc
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-09-12 21:36:16 +0300

    feat: use index.ts instead index.js in Button component

---
+++ src/components/button/index.ts
@@ -0,0 +1,18 @@
+import tpl from './tpl.hbs';
+import './style.scss';
+import { BaseBlock } from '../../utils/baseBlock';
+import { Props } from "./types";
+
+export class Button extends BaseBlock {
+  constructor(props: Props) {
+    super('div', props);
+  }
+
+  render() {
+    return tpl(this.props);
+  }
+}
+
+export function button(name, text, type = 'default') {
+  return tpl({ name, text, type });
+}
