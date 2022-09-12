2df262daf53147134e899b86452f5dffcdd4d5b9
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-09-13 00:51:15 +0300

    feat: Add RegExps and 'validate' function

---
+++ src/utils/validators/index.ts
@@ -0,0 +1,24 @@
+export const nameReg = /^[A-ZА-Я]+[a-zа-яA-ZА-Я-]{2,19}/;
+export const loginReg = /(?=.*[a-zA-Z])[a-zA-Z0-9\\_-]{2,19}/;
+export const emailReg = /^[\w\d]+@\w+\.\w+/;
+export const passwordReg = /^(?=.*[a-zа-я])(?=.*[A-ZА-Я])(?=.*\d)[a-zA-Zа-яА-я\d]{8,39}$/;
+export const phoneReg = /^\+?\d{9,15}$/;
+export const messageReg = /.*/;
+
+export const regExp = {
+  name: nameReg,
+  login: loginReg,
+  email: emailReg,
+  password: passwordReg,
+  phone: phoneReg,
+  message: messageReg,
+};
+
+type InputFieldName = 'name' | 'login' | 'email' | 'password' | 'phone' | 'message';
+
+export function validate(fieldName: InputFieldName, value: string) : boolean {
+  if (!(fieldName in regExp)) {
+    throw new Error('Указан несуществующий fieldName');
+  }
+  return regExp[fieldName].test(value);
+}
