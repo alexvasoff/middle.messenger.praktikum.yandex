ccfc1b1061888cc1055fe1e28aff95740d53ed0f
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-09-14 00:47:10 +0300

    feat: Rename function render to renderDOM

--- src/utils/renderDOM/index.ts
+++ src/utils/renderDOM/index.ts
@@ -1,6 +1,6 @@
 import { BaseBlock } from '../baseBlock';
 
-export function render(query, block: BaseBlock) {
+export function renderDOM(query, block: BaseBlock) {
   const root = document.querySelector(query);
   root.appendChild(block.getContent());
   block.dispatchComponentDidMount();
