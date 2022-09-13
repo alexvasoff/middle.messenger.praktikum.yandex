56aa57265efea022466de59404bd07b35501ed93
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-09-14 01:35:45 +0300

    feat: Make 'events' field non required in type of input

--- src/components/input/types.d.ts
+++ src/components/input/types.d.ts
@@ -3,7 +3,7 @@ import { BaseProps } from '../../utils/baseBlock';
 export type InputFieldName = 'name' | 'login' | 'email' | 'password' | 'phone' | 'message';
 
 export interface Props extends BaseProps {
-  events: { blur: () => void; focus: () => void };
+  events?: { blur: () => void; focus: () => void };
   name: InputFieldName;
   label: string;
   placeholder: string;
