4992360deaacf3c66da82628a8b1b3f236714b4c
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-09 18:10:27 +0300

    feat: Make chatList via component

---
+++ src/pages/chat/components/chatList/index.ts
@@ -0,0 +1,26 @@
+import tpl from './tpl.hbs';
+import './style.scss';
+
+import { Button } from '../../../../components/button';
+import { Input } from '../../../../components/input';
+import { BaseBlock } from '../../../../utils/baseBlock';
+
+class ChatList extends BaseBlock {
+  constructor(props) {
+    super('div', props);
+  }
+
+  render() {
+    return this.compile(tpl, this.props);
+  }
+}
+
+export function chatList() {
+  const props = {
+    settings: new Button({ name: 'profile', text: 'Настройки профиля' }),
+    createChat: new Button({ name: 'createChat', text: 'Создать чат', type: 'text' }),
+    search: new Input({ name: 'search', placeholder: 'Поиск' }),
+  };
+  const chatListComponent = new ChatList(props);
+  return chatListComponent;
+}
