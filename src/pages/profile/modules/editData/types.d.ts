1824580669532368ae54f6d5c7d401643ed0c7a3
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-09 18:44:34 +0300

    feat: Make editDat via component

---
+++ src/pages/profile/modules/editData/types.d.ts
@@ -0,0 +1,11 @@
+export interface Props {
+  avatar: unknown;
+  mail: unknown;
+  login: unknown;
+  name: unknown;
+  surname: unknown;
+  displayName: unknown;
+  phone: unknown;
+  savaChanges: unknown;
+  back: unknown;
+}
