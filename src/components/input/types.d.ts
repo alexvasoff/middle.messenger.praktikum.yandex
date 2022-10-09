58f8a0fd70bcd95eadeeb1b75241c4faf3d43ff5
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-09 18:16:24 +0300

    feat: expand InputFieldNames

--- src/components/input/types.d.ts
+++ src/components/input/types.d.ts
@@ -1,15 +1,15 @@
 import { BaseProps } from '../../utils/baseBlock';
 
 export type InputFieldName = 'name' | 'first_name' | 'second_name' | 'login' | 'email' |
-                             'password' | 'passwordGuard' | 'phone' | 'message';
+                             'password' | 'passwordGuard' | 'phone' | 'message' | 'search' | 'display_name';
 
 export interface Props extends BaseProps {
   events?: { blur: () => void; focus: () => void };
   name: InputFieldName;
-  label: string;
+  label?: string;
   placeholder: string;
   style?: {
-    width: string
-    readOnly: boolean
+    width?: string
+    readOnly?: boolean
   }
 }
