d6b4847019803825f2bad83a99e17950192fb789
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-13 00:30:35 +0300

    feat: Move props of chat page to its class

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -3,28 +3,23 @@ import './style.scss';
 import { BaseBlock } from '../../utils/baseBlock';
 import { chatContent } from './components/chatContent';
 import { chatList } from './components/chatList';
-import { renderDOM } from '../../utils/renderDOM';
 
 interface ChatPageProps {
-  [key: string] : any,
+  chatContent : unknown;
+  chatList : unknown;
 }
 
-class ChatPage extends BaseBlock {
-  constructor(props: ChatPageProps) {
-    super('div', props);
+const pageProps: ChatPageProps = {
+  chatContent: chatContent(),
+  chatList: chatList(),
+};
+
+export class ChatPage extends BaseBlock {
+  constructor() {
+    super('div', pageProps);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export function chat() {
-  const chatContentComponent = chatContent();
-  const chatListComponent = chatList();
-  const chatPage = new ChatPage({
-    chatContent: chatContentComponent,
-    chatList: chatListComponent,
-  });
-  return renderDOM('#root', chatPage);
 }
