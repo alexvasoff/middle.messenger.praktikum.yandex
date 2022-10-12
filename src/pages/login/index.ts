7a6bf70b4865e5a25f0bcfb5ffcf16f4fdcfdf18
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-13 00:30:46 +0300

    feat: Move props of Login page to its class

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -5,50 +5,44 @@ import { Props as ButtonProps } from '../../components/button/types';
 import { Input } from '../../components/input';
 import { Props as InputProps } from '../../components/input/types';
 import { Props as LoginPageProps } from './types';
-import { renderDOM } from '../../utils/renderDOM';
 import { BaseBlock } from '../../utils/baseBlock';
 
-class LoginPage extends BaseBlock {
-  constructor(props: LoginPageProps) {
-    super('div', props);
+const loginInputProps: InputProps = {
+  name: 'login',
+  label: 'Логин',
+  placeholder: 'Введите логин',
+};
+
+const passInputProps: InputProps = {
+  name: 'password',
+  label: 'Пароль',
+  placeholder: 'Введите пароль',
+};
+
+const signButtonProps: ButtonProps = {
+  name: 'sign',
+  text: 'Вход',
+};
+
+const registerButtonProps: ButtonProps = {
+  name: 'register',
+  text: 'Регистрация',
+  type: 'text',
+};
+
+const components: LoginPageProps = {
+  login: new Input(loginInputProps),
+  password: new Input(passInputProps),
+  sign: new Button(signButtonProps),
+  register: new Button(registerButtonProps),
+};
+
+export class LoginPage extends BaseBlock {
+  constructor() {
+    super('div', components);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export default function login() {
-  const loginInputProps: InputProps = {
-    name: 'login',
-    label: 'Логин',
-    placeholder: 'Введите логин',
-  };
-
-  const passInputProps: InputProps = {
-    name: 'password',
-    label: 'Пароль',
-    placeholder: 'Введите пароль',
-  };
-
-  const signButtonProps: ButtonProps = {
-    name: 'sign',
-    text: 'Вход',
-  };
-
-  const registerButtonProps: ButtonProps = {
-    name: 'register',
-    text: 'Регистрация',
-    type: 'text',
-  };
-
-  const components = {
-    login: new Input(loginInputProps),
-    password: new Input(passInputProps),
-    sign: new Button(signButtonProps),
-    register: new Button(registerButtonProps),
-  };
-
-  const loginPage = new LoginPage(components);
-  return renderDOM('#root', loginPage);
 }
