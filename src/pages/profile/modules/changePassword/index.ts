e92e047cb5e613dd3ff892f374e2f63c3bd253b4
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-13 00:31:02 +0300

    feat: Move props of ChangePass page to its class

--- src/pages/profile/modules/changePassword/index.ts
+++ src/pages/profile/modules/changePassword/index.ts
@@ -8,25 +8,21 @@ import { BaseBlock } from '../../../../utils/baseBlock';
 import { Props } from './types';
 import { renderDOM } from '../../../../utils/renderDOM';
 
-export class ProfileChangePassword extends BaseBlock {
-  constructor(props: Props) {
+const props: Props = {
+  avatar: new Avatar(),
+  oldPassword: new Input({ name: 'oldPassword', label: 'Старый пароль' }),
+  newPassword: new Input({ name: 'newPassword', label: 'Новый пароль' }),
+  newPasswordGuard: new Input({ name: 'newPasswordGuard', label: 'Повторите новый пароль' }),
+  savaChanges: new Button({ name: 'savaChanges', text: 'Сохранить изменения' }),
+  back: new Button({ name: 'back', text: 'Назад', type: 'danger-text' }),
+};
+
+export class ProfileChangePasswordPage extends BaseBlock {
+  constructor() {
     super('div', props);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export function changePassword() {
-  const props: Props = {
-    avatar: new Avatar(),
-    oldPassword: new Input({ name: 'oldPassword', label: 'Старый пароль' }),
-    newPassword: new Input({ name: 'newPassword', label: 'Новый пароль' }),
-    newPasswordGuard: new Input({ name: 'newPasswordGuard', label: 'Повторите новый пароль' }),
-    savaChanges: new Button({ name: 'savaChanges', text: 'Сохранить изменения' }),
-    back: new Button({ name: 'back', text: 'Назад', type: 'danger-text' }),
-  };
-  const changePasswordPage = new ProfileChangePassword(props);
-  return renderDOM('#root', changePasswordPage);
 }
