e4233d66547b8ead2cd7be3daa2267fa884e3621
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-13 00:31:58 +0300

    feat: Move props of Registration page to its class

--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -4,31 +4,25 @@ import { Button } from '../../components/button';
 import { Input } from '../../components/input';
 import { BaseBlock } from '../../utils/baseBlock';
 import { Props as RegistrationProps } from './types';
-import { renderDOM } from '../../utils/renderDOM';
 
-class Registration extends BaseBlock {
-  constructor(props: RegistrationProps) {
-    super('div', props);
+const components: RegistrationProps = {
+  mail: new Input({ name: 'email', label: 'Почта', placeholder: 'Введите почту' }),
+  login: new Input({ name: 'login', label: 'Логин', placeholder: 'Введите логин' }),
+  name: new Input({ name: 'first_name', label: 'Имя', placeholder: 'Введите имя' }),
+  surname: new Input({ name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию' }),
+  phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
+  password: new Input({ name: 'password', label: 'Пароль', placeholder: 'Введите пароль' }),
+  passwordGuard: new Input({ name: 'passwordGuard', label: 'Пароль (ещё раз)', placeholder: 'Повторите пароль' }),
+  registration: new Button({ name: 'registration', text: 'Регистрация' }),
+  sign: new Button({ name: 'sign', text: 'Вход', type: 'text' }),
+};
+
+export class RegistrationPage extends BaseBlock {
+  constructor() {
+    super('div', components);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export default function registration() {
-  const components = {
-    mail: new Input({ name: 'email', label: 'Почта', placeholder: 'Введите почту' }),
-    login: new Input({ name: 'login', label: 'Логин', placeholder: 'Введите логин' }),
-    name: new Input({ name: 'first_name', label: 'Имя', placeholder: 'Введите имя' }),
-    surname: new Input({ name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию' }),
-    phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
-    password: new Input({ name: 'password', label: 'Пароль', placeholder: 'Введите пароль' }),
-    passwordGuard: new Input({ name: 'passwordGuard', label: 'Пароль (ещё раз)', placeholder: 'Повторите пароль' }),
-    registration: new Button({ name: 'registration', text: 'Регистрация' }),
-    sign: new Button({ name: 'sign', text: 'Вход', type: 'text' }),
-  };
-
-  const registrationPage = new Registration(components);
-  return renderDOM('#root', registrationPage);
 }
