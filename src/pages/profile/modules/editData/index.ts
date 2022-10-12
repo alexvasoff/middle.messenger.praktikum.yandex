3ea2991ef53edcc744737e5460588fd4b6d9d45c
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-13 00:31:16 +0300

    feat: Move props of Profile page to its class

--- src/pages/profile/modules/editData/index.ts
+++ src/pages/profile/modules/editData/index.ts
@@ -5,30 +5,25 @@ import { Avatar } from '../../../../components/avatar';
 import { Input } from '../../../../components/input';
 import { Button } from '../../../../components/button';
 import { Props } from './types';
-import { renderDOM } from '../../../../utils/renderDOM';
 
-export class ProfileEditData extends BaseBlock {
-  constructor(props: Props) {
+const props: Props = {
+  avatar: new Avatar(),
+  mail: new Input({ name: 'email', label: 'Почта', placeholder: 'Введите почту' }),
+  login: new Input({ name: 'login', label: 'Логин', placeholder: 'Введите логин' }),
+  name: new Input({ name: 'first_name', label: 'Имя', placeholder: 'Введите имя' }),
+  surname: new Input({ name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию' }),
+  displayName: new Input({ name: 'display_name', label: 'Имя в чате', placeholder: 'Введите ник' }),
+  phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
+  savaChanges: new Button({ name: 'savaChanges', text: 'Сохранить изменения' }),
+  back: new Button({ name: 'back', text: 'Назад', type: 'danger-text' }),
+};
+
+export class ProfileEditDataPage extends BaseBlock {
+  constructor() {
     super('div', props);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export function editData() {
-  const props: Props = {
-    avatar: new Avatar(),
-    mail: new Input({ name: 'email', label: 'Почта', placeholder: 'Введите почту' }),
-    login: new Input({ name: 'login', label: 'Логин', placeholder: 'Введите логин' }),
-    name: new Input({ name: 'first_name', label: 'Имя', placeholder: 'Введите имя' }),
-    surname: new Input({ name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию' }),
-    displayName: new Input({ name: 'display_name', label: 'Имя в чате', placeholder: 'Введите ник' }),
-    phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
-    savaChanges: new Button({ name: 'savaChanges', text: 'Сохранить изменения' }),
-    back: new Button({ name: 'back', text: 'Назад', type: 'danger-text' }),
-  };
-  const editDataComponent = new ProfileEditData(props);
-  return renderDOM('#root', editDataComponent);
 }
--- src/pages/profile/modules/main/index.ts
+++ src/pages/profile/modules/main/index.ts
@@ -3,45 +3,40 @@ import './style.scss';
 import { BaseBlock } from '../../../../utils/baseBlock';
 import { Button } from '../../../../components/button';
 import { Input } from '../../../../components/input';
-import { renderDOM } from '../../../../utils/renderDOM';
 import { Avatar } from '../../../../components/avatar';
 
-export class ProfileMain extends BaseBlock {
-  constructor(props) {
-    super('div', props);
+const components = {
+  avatar: new Avatar(),
+  mail: new Input({
+    name: 'email', label: 'Почта', placeholder: 'Введите почту', style: { readOnly: true },
+  }),
+  login: new Input({
+    name: 'login', label: 'Логин', placeholder: 'Введите логин', style: { readOnly: true },
+  }),
+  name: new Input({
+    name: 'first_name', label: 'Имя', placeholder: 'Введите имя', style: { readOnly: true },
+  }),
+  surname: new Input({
+    name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию', style: { readOnly: true },
+  }),
+  displayName: new Input({
+    name: 'display_name', label: 'Имя в чате', placeholder: 'Введите ник', style: { readOnly: true },
+  }),
+  phone: new Input({
+    name: 'phone', label: 'Телефон', placeholder: 'Введите телефон', style: { readOnly: true },
+  }),
+  changeDate: new Button({ name: 'changeDate', text: 'Изменить данные' }),
+  changePassword: new Button({ name: 'changePassword', text: 'Изменить пароль' }),
+  back: new Button({ name: 'back', text: 'Назад', type: 'text' }),
+  exit: new Button({ name: 'exit', text: 'Выйти', type: 'danger-text' }),
+};
+
+export class ProfileMainPage extends BaseBlock {
+  constructor() {
+    super('div', components);
   }
 
   render() {
     return this.compile(tpl, this.props);
   }
-}
-
-export function main() {
-  const components = {
-    avatar: new Avatar(),
-    mail: new Input({
-      name: 'email', label: 'Почта', placeholder: 'Введите почту', style: { readOnly: true },
-    }),
-    login: new Input({
-      name: 'login', label: 'Логин', placeholder: 'Введите логин', style: { readOnly: true },
-    }),
-    name: new Input({
-      name: 'first_name', label: 'Имя', placeholder: 'Введите имя', style: { readOnly: true },
-    }),
-    surname: new Input({
-      name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию', style: { readOnly: true },
-    }),
-    displayName: new Input({
-      name: 'display_name', label: 'Имя в чате', placeholder: 'Введите ник', style: { readOnly: true },
-    }),
-    phone: new Input({
-      name: 'phone', label: 'Телефон', placeholder: 'Введите телефон', style: { readOnly: true },
-    }),
-    changeDate: new Button({ name: 'changeDate', text: 'Изменить данные' }),
-    changePassword: new Button({ name: 'changePassword', text: 'Изменить пароль' }),
-    back: new Button({ name: 'back', text: 'Назад', type: 'text' }),
-    exit: new Button({ name: 'exit', text: 'Выйти', type: 'danger-text' }),
-  };
-  const profileMainComponent = new ProfileMain(components);
-  return renderDOM('#root', profileMainComponent);
 }
