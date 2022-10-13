5ac319fb867e961c4a26d1cb950f5e6ee8b3669f
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-14 01:47:57 +0300

    feat: Add Api to Registration page

--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -4,6 +4,29 @@ import { Button } from '../../components/button';
 import { Input } from '../../components/input';
 import { BaseBlock } from '../../utils/baseBlock';
 import { Props as RegistrationProps } from './types';
+import { router } from '../../router';
+import { UserAuthController } from '../../utils/apiControllers/userAuth';
+
+async function onRegister() {
+  const inputFields = document.getElementsByTagName('input');
+  const params = {};
+  for (const inputField of inputFields) {
+    console.log(inputField);
+    params[inputField.name] = inputField.value;
+  }
+  const authApi = new UserAuthController();
+  const response = await authApi.signUp(params);
+  if (response.status !== 200) {
+    console.log('Не удалось создать пользователя');
+    console.log(response);
+    return;
+  }
+  router.go('/');
+}
+
+const events = {
+  click: onRegister,
+};
 
 const components: RegistrationProps = {
   mail: new Input({ name: 'email', label: 'Почта', placeholder: 'Введите почту' }),
@@ -13,7 +36,7 @@ const components: RegistrationProps = {
   phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
   password: new Input({ name: 'password', label: 'Пароль', placeholder: 'Введите пароль' }),
   passwordGuard: new Input({ name: 'passwordGuard', label: 'Пароль (ещё раз)', placeholder: 'Повторите пароль' }),
-  registration: new Button({ name: 'registration', text: 'Регистрация' }),
+  registration: new Button({ name: 'registration', text: 'Регистрация', events }),
   sign: new Button({ name: 'sign', text: 'Вход', type: 'text' }),
 };
 
