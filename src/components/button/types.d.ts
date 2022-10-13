67652427d2ea39eb44af1dc556e885a1bc318a95
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-14 01:51:23 +0300

    feat: Add Api to Profile page

--- src/components/button/types.d.ts
+++ src/components/button/types.d.ts
@@ -2,4 +2,5 @@ export type Props = {
   name: string;
   text: string;
   type?: 'default' | 'text' | 'danger-text';
+  events?: Record<string, unknown>;
 };
--- src/pages/profile/modules/main/index.ts
+++ src/pages/profile/modules/main/index.ts
@@ -4,6 +4,9 @@ import { BaseBlock } from '../../../../utils/baseBlock';
 import { Button } from '../../../../components/button';
 import { Input } from '../../../../components/input';
 import { Avatar } from '../../../../components/avatar';
+import { UserAuthController } from '../../../../utils/apiControllers/userAuth';
+import { store } from '../../../../utils/store';
+import { router } from '../../../../router';
 
 const components = {
   avatar: new Avatar(),
@@ -25,7 +28,15 @@ const components = {
   phone: new Input({
     name: 'phone', label: 'Телефон', placeholder: 'Введите телефон', style: { readOnly: true },
   }),
-  changeDate: new Button({ name: 'changeDate', text: 'Изменить данные' }),
+  changeDate: new Button({
+    name: 'changeDate',
+    text: 'Изменить данные',
+    events: {
+      click: () => {
+        router.go('/changeData');
+      },
+    },
+  }),
   changePassword: new Button({ name: 'changePassword', text: 'Изменить пароль' }),
   back: new Button({ name: 'back', text: 'Назад', type: 'text' }),
   exit: new Button({ name: 'exit', text: 'Выйти', type: 'danger-text' }),
@@ -34,6 +45,17 @@ const components = {
 export class ProfileMainPage extends BaseBlock {
   constructor() {
     super('div', components);
+    const authApi = new UserAuthController();
+    authApi.getInfo().then(userInfo => {
+      const inputFields = document.getElementsByTagName('input');
+      for (const inputField of inputFields) {
+        const fieldName = inputField.name;
+        if (!userInfo[fieldName]) {
+          continue;
+        }
+        inputField.value = userInfo[fieldName];
+      }
+    });
   }
 
   render() {
