ddf1104f7a25f4062710a3c16ccebcd2e4c3da9b
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-14 01:51:43 +0300

    feat: Add Api to EditData page

--- src/pages/profile/modules/editData/index.ts
+++ src/pages/profile/modules/editData/index.ts
@@ -5,6 +5,12 @@ import { Avatar } from '../../../../components/avatar';
 import { Input } from '../../../../components/input';
 import { Button } from '../../../../components/button';
 import { Props } from './types';
+import { router } from '../../../../router';
+import { UserAuthController } from '../../../../utils/apiControllers/userAuth';
+
+function goBack() {
+  router.back();
+}
 
 const props: Props = {
   avatar: new Avatar(),
@@ -14,13 +20,36 @@ const props: Props = {
   surname: new Input({ name: 'second_name', label: 'Фамилия', placeholder: 'Введите фамилию' }),
   displayName: new Input({ name: 'display_name', label: 'Имя в чате', placeholder: 'Введите ник' }),
   phone: new Input({ name: 'phone', label: 'Телефон', placeholder: 'Введите телефон' }),
-  savaChanges: new Button({ name: 'savaChanges', text: 'Сохранить изменения' }),
-  back: new Button({ name: 'back', text: 'Назад', type: 'danger-text' }),
+  savaChanges: new Button({
+    name: 'savaChanges',
+    text: 'Сохранить изменения',
+  }),
+  back: new Button({
+    name: 'back',
+    text: 'Назад',
+    type: 'danger-text',
+    events: {
+      click: goBack,
+    },
+  }),
 };
 
 export class ProfileEditDataPage extends BaseBlock {
   constructor() {
     super('div', props);
+    const authApi = new UserAuthController();
+    // тут ожидается, что данные уже в кэше (если перешли с основной страницы настроек)
+    authApi.getInfo().then(userInfo => {
+      console.log(userInfo);
+      const inputFields = document.getElementsByTagName('input');
+      for (const inputField of inputFields) {
+        const fieldName = inputField.name;
+        if (!userInfo[fieldName]) {
+          continue;
+        }
+        inputField.value = userInfo[fieldName];
+      }
+    });
   }
 
   render() {
