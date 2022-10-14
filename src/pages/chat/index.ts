84bf512a6d271d249e65ab1b3836355a2ab36d13
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-15 02:09:57 +0300

    feat: Add webSocket

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -4,7 +4,9 @@ import { BaseBlock } from '../../utils/baseBlock';
 import { chatContent } from './components/chatContent';
 import { chatList } from './components/chatList';
 import { ChatController } from '../../utils/apiControllers/chat';
+import { UserAuthController } from '../../utils/apiControllers/userAuth';
 
+const authApi = new UserAuthController();
 const chatApi = new ChatController();
 
 interface ChatPageProps {
@@ -26,8 +28,49 @@ export class ChatPage extends BaseBlock {
     return this.compile(tpl, this.props);
   }
 
-  openChat(chat) {
-    console.log('open chat', chat);
+  async openChat(chat) {
+    const response = await chatApi.getChatToken(chat.id);
+    if (response.status !== 200) {
+      console.log(('Не удалось получить токен чата'));
+      return;
+    }
+    const TOKEN_VALUE = response.response;
+    const USER_ID = await authApi.getInfo();
+    const CHAT_ID = chat.id;
+
+    if (!(TOKEN_VALUE && USER_ID && CHAT_ID)) {
+      console.log('Не все данные указаны');
+      console.log('TOKEN_VALUE, USER_ID, CHAT_ID:', TOKEN_VALUE, USER_ID, CHAT_ID);
+      return;
+    }
+
+    const socket = new WebSocket(`wss://ya-praktikum.tech/ws/chats/${USER_ID}>/${CHAT_ID}>/${TOKEN_VALUE}>`);
+    socket.addEventListener('open', () => {
+      console.log('Соединение установлено');
+
+      socket.send(JSON.stringify({
+        content: 'Моё первое сообщение миру!',
+        type: 'message',
+      }));
+    });
+
+    socket.addEventListener('close', event => {
+      if (event.wasClean) {
+        console.log('Соединение закрыто чисто');
+      } else {
+        console.log('Обрыв соединения');
+      }
+
+      console.log(`Код: ${event.code} | Причина: ${event.reason}`);
+    });
+
+    socket.addEventListener('message', event => {
+      console.log('Получены данные', event.data);
+    });
+
+    socket.addEventListener('error', event => {
+      console.log('Ошибка', event.message);
+    });
   }
 
   renderChats(chats) {
