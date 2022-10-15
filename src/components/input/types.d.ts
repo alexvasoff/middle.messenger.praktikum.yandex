545d81ae649e5ddb396ddb254571ea6765a521bb
Aleksey Vasilev <alexvasoff@yandex.ru>
2022-10-15 19:19:31 +0300

    fix: Move object properties to the next line

--- src/components/input/types.d.ts
+++ src/components/input/types.d.ts
@@ -1,7 +1,8 @@
 import { BaseProps } from '../../utils/baseBlock';
 
-export type InputFieldName = 'name' | 'first_name' | 'second_name' | 'login' | 'email' |
-                             'password' | 'passwordGuard' | 'phone' | 'message' | 'search' | 'display_name';
+export type InputFieldName =
+  'name' | 'first_name' | 'second_name' | 'login' | 'email' |
+  'password' | 'passwordGuard' | 'phone' | 'message' | 'search' | 'display_name';
 
 export interface Props extends BaseProps {
   events?: { blur: () => void; focus: () => void };
